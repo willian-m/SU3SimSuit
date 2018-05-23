1a2b45e27c07c7b7ee3202f7d00879f4f3013ff3
willian-m <not.public@not.public.com>
2018-05-23 15:05:42 -0300

    Adds script for running the lattice configuration generator

---
+++ scripts/run_gen_lat_conf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This script uses the following folder as root directory of the project
+DIR=`pwd`
+Ns=$1
+Nt=$2
+BETA=$3
+NMC=$4
+STEP=$5
+
+#--------------------------------------------
+mkdir -p $DIR/output
+mkdir -p $DIR/output/lat_conf
+cd $DIR/output/lat_conf
+$DIR/bin/gen_lat_conf.run $Ns $Ns $Ns $Nt $BETA H $NMC $STEP 
+cd $DIR
