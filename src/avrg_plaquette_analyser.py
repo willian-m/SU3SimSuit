7f3eeed1bea0675f32504950889121204122b22a
willian-m <not.public@not.public.com>
2018-06-11 11:28:51 -0300

    Adds jacknife analysis.
    Adds correlation time parameter (manual input) on the data analaysis.

--- src/avrg_plaquette_analyser.py
+++ src/avrg_plaquette_analyser.py
@@ -21,8 +21,8 @@ def auto_corr(d):
         out[-1] = out[-1] - avrg**2
     return(out)            
 
-#root_dir = "D:\\git_repos\\SU3SimSui"
-root_dir = "/home/willian/git_repos/SU3SimSuit"
+root_dir = "D:\\git_repos\\SU3SimSuit"
+#root_dir = "/home/willian/d/git_repos/SU3SimSuit"
 data=loadtxt(root_dir+'/output/avrg_plaquette.out')
 
 
@@ -30,17 +30,37 @@ plt.plot(range(len(data)),data)
 
 #input("Press enter to continue.")
 
-therm=1400 #Thermalization time
+therm=1 #Thermalization time
 
 c = auto_corr(data[therm:])
 plt.figure()
 plt.plot(range(len(c)),c)
 
-
+t=50
 plt.figure()
-plt.plot(range(len(data[therm:])),data[therm:])
-print(average(data[therm::10]),'+/-',std(data[therm::10])/sqrt((len(data)-therm)/60))
+plt.plot(range(len(data[therm::t])),data[therm::t])
+print(average(data[therm::t]),'+/-',std(data[therm::t])/sqrt((len(data[therm::t]))))
 
+def jacknife(data,nbins):
+    bin_data=np.zeros(nbins,dtype=np.float64)
+    bin_size=len(data)/nbins
+    for i in range(len(data)):
+        bin_index=int(i/bin_size)
+        bin_data[bin_index] = bin_data[bin_index] + data[i]
+    for i in range(nbins):
+        bin_data[i] = bin_data[i]/bin_size
+        
+    avrg=average(data)
+    error = 0.0
+    for i in range(nbins):
+        error=error+(bin_data[i]-avrg)**2
+    
+    error = error*(nbins-1.0)/nbins
+    return(error)
+        
+print(average(data[therm:]),'+/-',std(data[therm:])/sqrt((len(data)-therm)))
+
+print("Jacknife error:",jacknife(data[therm:],4))
 #plt.plot(range(len(data)),data)
 
 plt.show()
