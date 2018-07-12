329bc172ceba5ab6d5b5751331a45d6b6dd51b6e
willian-m <not.public@not.public.com>
2018-07-12 20:32:03 +0000

    Small changes to enable fast tests.

--- src/tensor_correlator.f90
+++ src/tensor_correlator.f90
@@ -7,16 +7,17 @@ use types_params
 use xml_parser, only : read_xml,mu,nu,rho,sigma
 implicit none
 character(len=1024) :: filename,list_of_files
-complex(dp), allocatable :: Tmunu(:,:,:), corr(:,:,:,:,:)
-integer :: record_len,x,nw,number_of_files,unit_list_files,x1,x2,t,k,s1,s2,file_num,t2
+complex(dp), allocatable :: Tmunu(:,:,:), corr(:)
+integer :: record_len,x,nw,number_of_files,unit_list_files,x1,x2,t,k,s1,s2,file_num,t2,mu_prime,nu_prime
 !load parameters (lattice size and lattice file name)
 call readargs() 
 
 !allocate vector that will hold tmunu
 allocate(tmunu(4,4,0:nx*ny*nz*nt-1))
-allocate(corr(4,4,4,4,0:nt-1))
+allocate(corr(0:nt-1))
 
 corr = CMPLX(0.0_dp,0.0_dp)
+print *, corr
 open(newunit=unit_list_files,file=list_of_files)
 
 !open file with list of data filenames
@@ -31,9 +32,9 @@ do file_num=1,number_of_files
     open(newunit=nw,file=filename,form="unformatted",access='direct',recl=record_len)
     !We load the file to memory
     do x=0,nx*ny*nz*nt-1
-        do mu=1,4
-            do nu=1,4
-                read(nw,rec=nu + 4*(mu-1) + 16*x) Tmunu(nu,mu,x)
+        do mu_prime=1,4
+            do nu_prime=1,4
+                read(nw,rec=nu_prime + 4*(mu_prime-1) + 16*x) Tmunu(nu_prime,mu_prime,x)
             end do
         end do
     end do
@@ -46,16 +47,27 @@ do file_num=1,number_of_files
     do t=0,nt-1
         do k=0,nt-1
             t2 = t + k - nt*((t+k)/nt)
-            do s1=0,nx*ny*nz-1
-                do s2=0,nx*ny*nz-1
+            s1=nx*ny*nz/8
+            s2=s1
+            !do s1=0,nx*ny*nz-1
+            !    do s2=0,nx*ny*nz-1
                     x1 = s1 + t*nx*ny*nz
-                    x2 = s2 + (t+k -nt*((k+t)/nt))*nx*ny*nz
-                    corr(rho,sigma,mu,nu,k) = corr(rho,sigma,mu,nu,k) + Tmunu(mu,nu,x1)*Tmunu(rho,sigma,x2)
-                end do
-            end do
+                    x2 = s2 + t2*nx*ny*nz
+                    if (real(Tmunu(mu,nu,x1)) .lt. 0.0_dp .or. real(Tmunu(rho,sigma,x2)) .lt. 0.0_dp) then
+                        print *, t,x1,t2,x2,real(Tmunu(mu,nu,x1)),real(Tmunu(rho,sigma,x2))
+                    end if
+                    if (abs(imag(Tmunu(mu,nu,x1))) .gt. 1.0e-15_dp  .or. abs(imag(Tmunu(rho,sigma,x2))) .gt. 1.0e-15_dp ) then
+                        print *, "Error! Found complex tensor. Results unreliable. Aborting."
+                        call exit(-1)
+                    else
+                        corr(k) = corr(k) + real(Tmunu(mu,nu,x1))*real(Tmunu(rho,sigma,x2))
+                    end if
+            !    end do
+            !end do
         end do
     end do
     print *, "Correlation for file ",trim(filename)," completed."
+    !read(5,*)
 end do
 close(unit_list_files)
 
@@ -63,16 +75,10 @@ print *, "Corr computed"
 !Normalization: 
 corr = corr/(real(nt*number_of_files)*real(nx*ny*nz)**2)
 
-mu=4
-nu=1
-rho=mu
-sigma=nu
-corr(rho,sigma,mu,nu,k) = corr(rho,sigma,mu,nu,k) + Tmunu(mu,nu,x1)*Tmunu(rho,sigma,x2)
-
 write(filename,'("Corr",4I1.1,".dat")') mu,nu,rho,sigma
 open(newunit=nw,file=filename)
 do t=0,nt-1
-    write(nw,*) t, real(corr(mu,nu,rho,sigma,t)), imag(corr(mu,nu,rho,sigma,t))
+    write(nw,*) t, real(corr(t)), imag(corr(t))
 end do
 close(nw)
 
