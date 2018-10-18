91c04052127c9a25216fbd130247fc1434105346
willian-m <not.public@not.public.com>
2018-10-18 11:20:27 -0300

    Fix crash and NaN error.

--- src/modules/fermion_prop.f90
+++ src/modules/fermion_prop.f90
@@ -62,13 +62,13 @@ module fermion_prop
       do alpha=1,4
          do a=1,3
             spin_color_index = a + 3*(alpha-1)
-            call zcopy(array_dim,source(:,spin_color_index),i_one,prop(:,spin_color_index),1)
+            call zcopy(array_dim,source(:,spin_color_index),i_one,prop(:,spin_color_index),i_one)
             
             !Initialization
-            call dirac_vector_multiply(prop(:,spin_color_index),array_dim,r)
+            call dirac_vector_multiply(prop(:,spin_color_index),r)
             call zaxpy(array_dim,z_minus_one,source(:,spin_color_index),i_one,r,i_one) !constant times a vector plus a vector
-            call zscal(array_dim,z_minus_one,r,1) !scales a vector by a constant
-            call zcopy(array_dim,r,i_one,r_tilde,1)
+            call zscal(array_dim,z_minus_one,r,i_one) !scales a vector by a constant
+            call zcopy(array_dim,r,i_one,r_tilde,i_one)
             
             stop_condition = .true.
             step = 1
@@ -76,15 +76,15 @@ module fermion_prop
                !Begin computes p
                if (step .eq. 1) then
                   rho1 = zdotc(array_dim,r_tilde,i_one,r,i_one)   !Needed for next step
-                  call zcopy(array_dim,r,i_one,p,1)
+                  call zcopy(array_dim,r,i_one,p,i_one)
                else
                   rho0 = rho1
                   rho1 = zdotc(array_dim,r_tilde,i_one,r,i_one)
                   if ( sqrt( real(rho1)**2 + aimag(rho1)**2 ) > tol) then
                      beta_cg = alpha_cg*rho1/(omega*rho0)
-                     call zaxpy(array_dim,-omega,v,i_one,p,1)
-                     call zscal(array_dim,beta_cg,p,1)
-                     call zaxpy(array_dim,z_one,r,i_one,p,1)
+                     call zaxpy(array_dim,-omega,v,i_one,p,i_one)
+                     call zscal(array_dim,beta_cg,p,i_one)
+                     call zaxpy(array_dim,z_one,r,i_one,p,i_one)
                   else
                      print *, "ERROR: Division by zero found. Aborting"
                      stop_condition = .false.
@@ -92,34 +92,31 @@ module fermion_prop
                end if
 
                !TO DO: In case of preconditioned algorithm, invert M matrix and find a p_hat
-               !End compute p
-               !Begin compute s
-               print *, "Begin compute s"
-               call dirac_vector_multiply(p,array_dim,v) !v(i) = A p(i)
+               call dirac_vector_multiply(p,v) !v(i) = A p(i)
                
                alpha_cg = zdotc(array_dim,r_tilde,i_one,v,i_one)
                alpha_cg=rho1/alpha_cg
-               call zaxpy(array_dim,-alpha_cg,v,i_one,r,1) !r = s for now. Later, it will be discarded and a new r computed
+               call zaxpy(array_dim,-alpha_cg,v,i_one,r,i_one) !r = s for now. Later, it will be discarded and a new r computed
                !End compute s
-               s_norm = DZnrm2(array_dim,r,1)
+               s_norm = DZnrm2(array_dim,r,i_one)
                if (s_norm .gt. tol) then
                   !TO DO: In case of preconditioned algorithm, invert matrix M and find s_hat
-                  call dirac_vector_multiply(r,array_dim,t)
-                  s_norm = DZnrm2(array_dim,t,1) !t_dagger x t stored in s_norm
+                  call dirac_vector_multiply(r,t)
+                  s_norm = DZnrm2(array_dim,t,i_one) !t_dagger x t stored in s_norm
                   omega = zdotc(array_dim,t,i_one,r,i_one) !t_dagger x s stored in omega
                   omega = omega/(s_norm**2) ! set omega
                   !Update propagator
-                  call zaxpy(array_dim,alpha_cg,p,i_one,prop(:,spin_color_index),1)
-                  call zaxpy( array_dim,omega,r,i_one,prop(:,spin_color_index),1)
+                  call zaxpy(array_dim,alpha_cg,p,i_one,prop(:,spin_color_index),i_one)
+                  call zaxpy( array_dim,omega,r,i_one,prop(:,spin_color_index),i_one)
                   !Compute r for next iteration
-                  call zaxpy(array_dim,-omega,t,i_one,r,1)
+                  call zaxpy(array_dim,-omega,t,i_one,r,i_one)
                   step = step + 1
                   if (step .gt. max_step) then
                      print *, "Max step reached without finding a convergence"
                      stop_condition = .false.
                   end if
                else
-                  call zaxpy(array_dim,alpha_cg,p,i_one,prop(:,spin_color_index),1)
+                  call zaxpy(array_dim,alpha_cg,p,i_one,prop(:,spin_color_index),i_one)
                   stop_condition = .false.
                end if
             end do
@@ -138,28 +135,21 @@ module fermion_prop
    
    !=============================
    !Multiply a vector by the dirac operator
-   subroutine dirac_vector_multiply(in_array,array_dim,out_array)
+   subroutine dirac_vector_multiply(in_array,out_array)
       complex(dp), intent(in) :: in_array(:)
       complex(dp), intent(out) :: out_array(:)
-      
-      integer, intent(in) :: array_dim
-      integer :: i, beta,b, istat
-      
-      !type(cublasHandle) :: handler
-      !istat = cublasCreate(handler)
+      integer :: i, beta,b
       
       do i=0,array_dim/12-1
-        do beta=1,4
+        do beta=1,4 
             do b=1,3
+               dirac_vector = cmplx(0.0_dp,0.0_dp)
                call make_dirac_vector(b,beta,i)
                out_array(b + 3*(beta-1) + 12*i) = zdotu(array_dim,dirac_vector,i_one,in_array,i_one)
             end do
          end do
       end do
-    
-      !deallocate(dirac_vector)
-      !istat = cublasDestroy(handler)
-      !print *, "Handler created."
+
    end subroutine
    !=============================
 
@@ -175,13 +165,13 @@ module fermion_prop
                beta = (i-1)/3
                b = i - beta*3
       
-               dirac_vector(b+3*(beta-1) + 12*y) = cmplx(0.0_dp,0.0_dp)
+               dirac_vector(b+3*beta + 12*y) = cmplx(0.0_dp,0.0_dp)
                if (y .eq. increment_table(x,mu)) then
-                  dirac_vector(b+3*(beta-1) + 12*y) = dirac_vector(b+3*(beta-1) + 12*y) + gamma_mu_star(alpha,beta,mu)*U(mu,x)%a(a,b)
+                  dirac_vector(b+3*beta + 12*y) = dirac_vector(b+3*beta + 12*y) + gamma_mu_star(alpha,beta+1,mu)*U(mu,x)%a(a,b)
                end if
 
-               if ( (y .eq. x) .and. (a .eq. b) .and. (alpha .eq. beta) ) then
-                  dirac_vector(b+3*(beta-1) + 12*y) = dirac_vector(b+3*(beta-1) + 12*y) +  cmplx(4.0_dp + mass,0.0_dp)
+               if ( (y .eq. x) .and. (a .eq. b) .and. (alpha .eq. beta+1) ) then
+                  dirac_vector(b+3*beta + 12*y) = dirac_vector(b+3*beta + 12*y) +  cmplx(4.0_dp + mass,0.0_dp)
                end if
             end do
          end do
@@ -258,43 +248,43 @@ module fermion_prop
 
 
       !gamma_muamma_1
-      gamma_mu(0,3,2) = dcmplx(0.0_dp,1.0_dp)
-      gamma_mu(1,2,2) = dcmplx(0.0_dp,1.0_dp)
-      gamma_mu(2,1,2) = dcmplx(0.0_dp,-1.0_dp)
-      gamma_mu(3,0,2) = dcmplx(0.0_dp,-1.0_dp)
+      gamma_mu(1,4,2) = dcmplx(0.0_dp,1.0_dp)
+      gamma_mu(2,3,2) = dcmplx(0.0_dp,1.0_dp)
+      gamma_mu(3,2,2) = dcmplx(0.0_dp,-1.0_dp)
+      gamma_mu(4,1,2) = dcmplx(0.0_dp,-1.0_dp)
 
       gamma_mu(:,:,1) = -gamma_mu(:,:,2)!gamma_mu_{-mu} = -gamma_mu_mu.
 
       !gamma_muamma_2
-      gamma_mu(0,3,4) = dcmplx(1.0_dp,0.0_dp)
-      gamma_mu(1,2,4) = dcmplx(-1.0_dp,0.0_dp)
-      gamma_mu(2,1,4) = dcmplx(-1.0_dp,0.0_dp)
-      gamma_mu(3,0,4) = dcmplx(1.0_dp,0.0_dp)
+      gamma_mu(1,4,4) = dcmplx(1.0_dp,0.0_dp)
+      gamma_mu(2,3,4) = dcmplx(-1.0_dp,0.0_dp)
+      gamma_mu(3,2,4) = dcmplx(-1.0_dp,0.0_dp)
+      gamma_mu(4,1,4) = dcmplx(1.0_dp,0.0_dp)
 
       gamma_mu(:,:,3) = -gamma_mu(:,:,4)!gamma_mu_{-mu} = -gamma_mu_mu.
 
       !gamma_muamma_3
-      gamma_mu(0,2,6) = dcmplx(0.0_dp,1.0_dp)
-      gamma_mu(1,3,6) = dcmplx(0.0_dp,-1.0_dp)
-      gamma_mu(2,0,6) = dcmplx(0.0_dp,-1.0_dp)
-      gamma_mu(3,1,6) = dcmplx(0.0_dp,1.0_dp)
+      gamma_mu(1,3,6) = dcmplx(0.0_dp,1.0_dp)
+      gamma_mu(2,4,6) = dcmplx(0.0_dp,-1.0_dp)
+      gamma_mu(3,1,6) = dcmplx(0.0_dp,-1.0_dp)
+      gamma_mu(4,2,6) = dcmplx(0.0_dp,1.0_dp)
 
       gamma_mu(:,:,5) = -gamma_mu(:,:,6)!gamma_mu_{-mu} = -gamma_mu_mu.
 
       !gamma_muamma_4
-      gamma_mu(0,2,8) = dcmplx(1.0_dp,0.0_dp)
       gamma_mu(1,3,8) = dcmplx(1.0_dp,0.0_dp)
-      gamma_mu(2,0,8) = dcmplx(1.0_dp,0.0_dp)
+      gamma_mu(2,4,8) = dcmplx(1.0_dp,0.0_dp)
       gamma_mu(3,1,8) = dcmplx(1.0_dp,0.0_dp)
+      gamma_mu(4,2,8) = dcmplx(1.0_dp,0.0_dp)
 
       gamma_mu(:,:,7) = -gamma_mu(:,:,8)!gamma_mu_{-mu} = -gamma_mu_mu.
 
-      !gamma_mu_star is the negamma_muative of gamma_mu
+      !gamma_mu_star is the negative of gamma_mu
       gamma_mu_star = -gamma_mu
-      !except on the diagamma_muonal components
+      !except on the diagonal components
       do mu=1,8
          do alpha=1,4
-         gamma_mu_star(alpha,alpha,mu) = 1.0_dp - gamma_mu(alpha,alpha,mu)
+         gamma_mu_star(alpha,alpha,mu) = cmplx(1.0_dp,0.0_dp) - gamma_mu(alpha,alpha,mu)
          end do
       end do
 
